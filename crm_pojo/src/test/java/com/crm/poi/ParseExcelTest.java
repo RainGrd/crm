8e568940bfa1e381bf18415f891f3cc3b78007c8
RainGrd <3408396469@qq.com>
2022-09-06 08:33:18 +0800

    nine commit

--- crm_pojo/src/test/java/com/crm/poi/ParseExcelTest.java
+++ crm_pojo/src/test/java/com/crm/poi/ParseExcelTest.java
@@ -1,13 +1,11 @@
 package com.crm.poi;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
 
 /**
  * Copyright (C), 2017-2022, RainGrd
@@ -17,15 +15,33 @@ import java.io.InputStream;
  * Description: 使用poi解析Excel文件
  */
 public class ParseExcelTest {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         /**/
         FileInputStream fileInputStream = new FileInputStream("D:\\lenovo\\Desktop\\activityList.xls");
-        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFWorkbook workbook = new HSSFWorkbook(fileInputStream);
         /*根据workbook对象获取HSSFSheet对象 页的下标，下标从零开始，一次增加*/
         HSSFSheet sheet = workbook.getSheetAt(0);
-        for (int i = 0; i < 10; i++) {
-            
+        HSSFRow row = null;
+        HSSFCell cell = null;
+        for (int i = 0; i <=sheet.getLastRowNum(); i++) {
+            /*行的下标，下标从零开始，依次增加*/
+            row = sheet.getRow(i);
+            for (int j = 0; j < row.getLastCellNum(); j++) {
+                cell = row.getCell(j);
+                /*获取列中的数据*/
+                if (cell.getCellType()== HSSFCell.CELL_TYPE_STRING) {
+                    System.out.print(cell.getStringCellValue()+" ");
+                }else if (cell.getCellType()== HSSFCell.CELL_TYPE_NUMERIC) {
+                    System.out.print(cell.getNumericCellValue()+" ");
+                } else if (cell.getCellType()== HSSFCell.CELL_TYPE_BOOLEAN) {
+                    System.out.print(cell.getBooleanCellValue()+" ");
+                }else if (cell.getCellType()== HSSFCell.CELL_TYPE_STRING) {
+                    System.out.print(cell.getCellFormula()+" ");
+                }else {
+                    System.out.print(" "+" ");
+                }
+            }
+            System.out.println();
         }
-
     }
 }
